b6852dcb3812b666b3760641a07155545f8f385a
Om Krishna <omkrishna.ok@gmail.com>
2020-12-19 10:52:06 +0530

    vikas' recommendations

--- js/start.js
+++ js/start.js
@@ -3,4 +3,8 @@ function ToggleStartNavigation(event) {
   var sel = document.querySelector(".selected");
   sel.classList.remove("selected");
   elem.classList.add("selected");
+  var elem_str = elem.children[0].innerHTML.toLowerCase() + "-container";
+  var sel_str = sel.children[0].innerHTML.toLowerCase() + "-container";
+  document.getElementById(sel_str).style.display = "none";
+  document.getElementById(elem_str).style.display = "flex";
 }
